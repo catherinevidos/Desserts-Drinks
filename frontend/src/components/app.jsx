76d77395e2dc620d0c484a7145e9c64cc1c01b5e
cvidos <catherine.vidos@gmail.com>
2020-10-05 14:35:53 -0400

    add app and root

---
+++ frontend/src/components/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  AuthRoute,
+  ProtectedRoute
+} from '../util/route_util';
+import {
+  Switch
+} from 'react-router-dom';
+
+
+import LoginContainer from './login/login_container';
+import SignupContainer from './signup/signup_container';
+
+const App = () => ( 
+  <div>
+    <Switch >
+      <AuthRoute exact path='/' component={Splash}/> 
+      <AuthRoute exact path='/login' component={LoginContainer}/>
+      <AuthRoute exact path='/signup' component={SignupContainer}/>
+    </Switch> 
+  </div>
+
+);
+
+export default App;
