211ac1c3df0aca13caaa7d24b115e10545ba9122
Serendipitydeus <calixto_jonathan@ymail.com>
2020-10-05 12:49:06 -0400

    added additional reducers, store

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -5,6 +5,7 @@ export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const LOGIN_NEW_USER = 'LOGIN_NEW_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const CLEAR_SESSION_ERRORS = 'CLEAR_SESSION_ERRORS';
 
 export const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
@@ -22,6 +23,10 @@ export const logoutCurrentUser = () => ({
 export const receiveErrors = errors => ({
     type: RECEIVE_SESSION_ERRORS,
     errors
+});
+
+export const clearErrors = () => ({
+    type: CLEAR_SESSION_ERRORS
 });
 
 export const signup = user => dispatch (
