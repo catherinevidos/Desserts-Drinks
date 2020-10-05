7912f11df445d3c3086ab5b4dcea20283b65da1f
Serendipitydeus <calixto_jonathan@ymail.com>
2020-10-05 10:38:20 -0400

    removed extra passport file

--- config/passport.js
+++ config/passport.js
@@ -3,19 +3,16 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const passport = require('passport');
 const User = mongoose.model('User');
-const keys = require('../config/keys');
-
-
+const keys = require('./keys');
 const options = {};
+
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = keys.secretOrKey;
-
 module.exports = passport => {
     passport.use(new JwtStrategy(options, (jwt_payload, done) => {
         User.findById(jwt_payload.id)
             .then(user => {
-
-                if(user) {
+                if (user) {
                     return done(null, user);
                 }
                 return done(null, false);
