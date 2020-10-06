921f1c2d1c588f5f8ed21f11d68837564cc60147
Serendipitydeus <calixto_jonathan@ymail.com>
2020-10-05 22:41:03 -0400

    fixed packages

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -5,7 +5,7 @@ import {
 import {
   Switch
 } from 'react-router-dom';
-import WebMap from "./components/map2";
+// import WebMap from "./components/map2";
 
 
 import LoginContainer from './login/login_container';
@@ -13,13 +13,13 @@ import SignupContainer from './signup/signup_container';
 import SplashContainer from './splash/splash_container';
 
 const App = () => (
-  <div>
+
     <Switch>
       <AuthRoute exact path="/" component={SplashContainer} />
       <AuthRoute exact path="/login" component={LoginContainer} />
       <AuthRoute exact path="/signup" component={SignupContainer} />
     </Switch>
-  </div>
+
 );
 
 export default App;
