9fb37c06c03fb9258410ac0af987353171e8b7b5
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2020-10-07 17:00:39 -0400

    maps updated

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,11 +1,13 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://dbAdmin:94XAF95TbHahjy3G@cluster0.u9tu2.mongodb.net/Desserts&Drinks?retryWrites=true&w=majority",
+    "mongodb+srv://dbAdmin:tLnrOWseqgVtBNXX@cluster0.u9tu2.mongodb.net/Desserts&Drinks?retryWrites=true&w=majority",
+
   secretOrKey: "secret",
+  
   aws: {
-    accessKeyId: "AKIA3LWHDXUQSM6LW54E",
+    accessKeyId: "AKIA3LWHDXUQRC7W4DXB",
     uploadedFileURL: "https://dessertanddrinks-dev.s3.amazonaws.com/",
-    secretAccessKey: "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",
+    secretAccessKey: "CbCKDoNc4g9g2QzKuHG+9HAy8DuUK9E4zrSbbn4Z",
     region: "us-west-1",
     bucketName: "dessertanddrinks-dev",
   },
