90efbd35dc99b43457823ae61eca95ffb155c93b
Adel Qudsi <AD3LLQ101@GMAIL.COM>
2020-10-06 23:48:09 -0400

    yelp api

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -4,17 +4,36 @@ import {
   GoogleApiWrapper,
   Marker
 } from 'google-maps-react';
+import YelpAPI from '../yelp/yelp_api';
 const googleMapApiKey = require("../../config/secret").googleMapApiKey;
 
+
 export class WebMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: '',
+      lng: '',
+      openModal: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchAllStops();
   }
+
+  handleClick(e){
+    this.setState({
+      lat: e.position.lat,
+      lng: e.position.lng,
+      openModal: true
+    })
+  }
  
   render() {
-    debugger
     if (this.props.stops.length === 0) return null;
+    
     const style = {
       width: "500px",
       height: "1000px",
@@ -23,26 +42,34 @@ export class WebMap extends React.Component {
     const { google } = this.props;
     console.log(this.props);
     return (
-      <Map
-        google={this.props.google}
-        style={style}
-        zoom={13}
-        initialCenter={{ lat: 40.7678805, lng: -73.97103059999999 }}
-      >
-        {this.props.stops[0].map((stop, i) => (
-          <Marker
-            key={`${i}-${stop.id}`}
-            title={stop.name}
-            position={{ lat: stop.lat, lng: stop.lng }}
-            icon={{
-              url:
-                "https://bestfriend-treehouse-dev.s3.amazonaws.com/Marker_logo.png",
-              anchor: new google.maps.Point(32, 32),
-              scaledSize: new google.maps.Size(75, 75),
-            }}
-          />
-        ))}
-      </Map>
+      <div className='map'>
+        <Map
+          google={this.props.google}
+          style={style}
+          zoom={13}
+          initialCenter={{ lat: 40.7678805, lng: -73.97103059999999 }}
+        >
+          {this.props.stops[0].map((stop, i) => (
+            <Marker
+              key={`${i}-${stop.id}`}
+              title={stop.name}
+              position={{ lat: stop.lat, lng: stop.lng }}
+              onClick={this.handleClick}
+              icon={{
+                url:
+                  "https://bestfriend-treehouse-dev.s3.amazonaws.com/Untitled+design.png",
+                scaledSize: new google.maps.Size(30, 30),
+              }}
+            />
+          ))}
+        </Map>
+        {this.state.openModal ? 
+          <YelpAPI 
+            lat={this.state.lat} 
+            lng={this.state.lng}
+          /> 
+        : null}
+      </div>
     );
   }
 }
--- frontend/src/config/secret.js
+++ frontend/src/config/secret.js
@@ -1,3 +1,8 @@
 module.exports = {
   googleMapApiKey: "AIzaSyDs-NGivXXoF3lKr7jnXkvN93CI9U7wiiM",
+
+  API_BASE_URL: 'http://api.yelp.com/v3',
+  
+  yelpApiKey:
+    "ND9GH1BpIv7rxOouoM6m4W275pbt-0Mk49L2LPHbZ5qWRVP6fT_jcc6A76tstz7q4HjwwQBN60G2nESyWQBPF5zUCA1hnRcB8qR5bQUCQWS96a6Os9lhc-Rk17B8X3Yx",
 };
