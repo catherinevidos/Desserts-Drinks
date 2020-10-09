e153e2835fb4926228da65f4ef18f5fe1d0e04b6
cvidos <catherine.vidos@gmail.com>
2020-10-08 21:52:40 -0400

    fixing yelp

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const cors = require("cors");
 app.use(cors());
 const path = require("path");
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
