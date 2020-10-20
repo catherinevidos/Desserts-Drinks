d79587c3b8058fa1edb40c94da1323c7e9442c52
cvidos <catherine.vidos@gmail.com>
2020-10-20 13:53:14 -0400

    styling and font changes

--- frontend/src/components/comment/comment_form.jsx
+++ frontend/src/components/comment/comment_form.jsx
@@ -21,19 +21,45 @@ export default class CommentForm extends React.Component {
             user_id: this.state.user_id,
             username: this.state.username
         };
-        this.props.createComment(comment);
+        this.props.createComment(comment).then(() => this.setState(this.props.comment))
     }
 
     render(){
 
         return(
             <div className='comments-wrapper'>
-                <div>
-                    <h1>Add a Comment!</h1>
+                <div className='comments-header'>
+                    <h1>Leave a Comment</h1>
                 </div>
-                <form onSubmit={this.handleSubmit}>
-                    Username: <label>{this.state.username}</label>
-                    <label>Comment:
+                <form  className=
+                'comment-form' onSubmit={this.handleSubmit}>
+                    <div className='username-field'>
+                    Username: 
+                    <label>{this.state.username}</label>
+                    </div>
+                    <br></br>
+                      <fieldset class="rating">
+                        <legend>Rating:</legend>
+                        <input type="radio" id="star5" name="rating" value="5" onChange={this.update('rating')} /><label for="star5" title="Rocks!">5 stars</label>
+                        <input type="radio" id="star4" name="rating" value="4" onChange={this.update('rating')} /><label for="star4" title="Pretty good">4 stars</label>
+                        <input type="radio" id="star3" name="rating" value="3" onChange={this.update('rating')} /><label for="star3" title="Meh">3 stars</label>
+                        <input type="radio" id="star2" name="rating" value="2" onChange={this.update('rating')} /><label for="star2" title="Kinda bad">2 stars</label>
+                        <input type="radio" id="star1" name="rating" value="1" onChange={this.update('rating')} /><label for="star1" title="Sucks big time">1 star</label>
+                        </fieldset>
+                        <br></br>
+                        {/* <input 
+                            type="number"
+                            value={this.state.rating}
+                            onChange={this.update('rating')}
+                            required
+                            max='5'
+                            placeholder='5'
+                        /> */}
+                    <br></br>
+                    <label className='comment-textarea-label'>Comment:
+                    </label>
+                    <br></br>
+                     <br></br>
                         <textarea 
                             cols="40" 
                             rows="3"
@@ -41,18 +67,11 @@ export default class CommentForm extends React.Component {
                             onChange={this.update('body')}
                             placeholder='Tell everyone about your experiences!'
                             required
+                            className='comment-textarea'
                         />
-                    </label>
-                    <label>Rating:
-                        <input 
-                            type="number"
-                            value={this.state.rating}
-                            onChange={this.update('rating')}
-                            required
-                            max='5'
-                        />
-                    </label>
-                    <button type='submit'>Create Comment</button>
+                    <br></br>
+                    <button className=
+                    'comment-submit' type='submit'>Submit Comment</button>
                 </form>
             </div>
         )
