1e21df1ffb28edba104c5e82aa2400e13f4c4916
rachanavishwanath <rachana.gujrathi@gmail.com>
2020-10-20 17:43:23 -0700

    Added backend route to delete a comment

--- frontend/src/components/comment/comment_form.jsx
+++ frontend/src/components/comment/comment_form.jsx
@@ -42,30 +42,74 @@ export default class CommentForm extends React.Component {
             return [];
         }
 
-        return(
-            <div className='comments-wrapper'>
-                <div className='comments-header-wrapper'>
-                    <div className='comments-header'>
-                        <h1>Leave a Comment</h1>
-                    </div>
-                </div>
-                <form  className=
-                'comment-form' onSubmit={this.handleSubmit}>
-                    <div className='username-field'>
-                    Username: 
-                    <label>{this.state.username}</label>
-                    </div>
-                    <br></br>
-                      <fieldset className="rating">
-                        <legend>Rating:</legend>
-                        <input type="radio" id="star5" name="rating" value="5" onChange={this.update('rating')} /><label for="star5" title="Rocks!">5 stars</label>
-                        <input type="radio" id="star4" name="rating" value="4" onChange={this.update('rating')} /><label for="star4" title="Pretty good">4 stars</label>
-                        <input type="radio" id="star3" name="rating" value="3" onChange={this.update('rating')} /><label for="star3" title="Meh">3 stars</label>
-                        <input type="radio" id="star2" name="rating" value="2" onChange={this.update('rating')} /><label for="star2" title="Kinda bad">2 stars</label>
-                        <input type="radio" id="star1" name="rating" value="1" onChange={this.update('rating')} /><label for="star1" title="Sucks big time">1 star</label>
-                        </fieldset>
-                        <br></br>
-                        {/* <input 
+        return (
+          <div className="comments-wrapper">
+            <div className="comments-header-wrapper">
+              <div className="comments-header">
+                <h1>Leave a Comment</h1>
+              </div>
+            </div>
+            <form className="comment-form" onSubmit={this.handleSubmit}>
+              <div className="username-field">
+                Username:
+                <label>{this.state.username}</label>
+              </div>
+              <br></br>
+              <fieldset className="rating">
+                <legend>Rating:</legend>
+                <input
+                  type="radio"
+                  id="star5"
+                  name="rating"
+                  value="5"
+                  onChange={this.update("rating")}
+                />
+                <label for="star5" title="Rocks!">
+                  5 stars
+                </label>
+                <input
+                  type="radio"
+                  id="star4"
+                  name="rating"
+                  value="4"
+                  onChange={this.update("rating")}
+                />
+                <label for="star4" title="Pretty good">
+                  4 stars
+                </label>
+                <input
+                  type="radio"
+                  id="star3"
+                  name="rating"
+                  value="3"
+                  onChange={this.update("rating")}
+                />
+                <label for="star3" title="Meh">
+                  3 stars
+                </label>
+                <input
+                  type="radio"
+                  id="star2"
+                  name="rating"
+                  value="2"
+                  onChange={this.update("rating")}
+                />
+                <label for="star2" title="Kinda bad">
+                  2 stars
+                </label>
+                <input
+                  type="radio"
+                  id="star1"
+                  name="rating"
+                  value="1"
+                  onChange={this.update("rating")}
+                />
+                <label for="star1" title="Sucks big time">
+                  1 star
+                </label>
+              </fieldset>
+              <br></br>
+              {/* <input 
                             type="number"
                             value={this.state.rating}
                             onChange={this.update('rating')}
@@ -73,39 +117,40 @@ export default class CommentForm extends React.Component {
                             max='5'
                             placeholder='5'
                         /> */}
-                    <br></br>
-                    <label className='comment-textarea-label'>Comment:
-                    </label>
-                    <br></br>
-                     <br></br>
-                        <textarea 
-                            cols="40" 
-                            rows="3"
-                            value={this.state.body}
-                            onChange={this.update('body')}
-                            placeholder='Tell everyone about your experiences!'
-                            required
-                            className='comment-textarea'
-                        />
-                    <br></br>
-                    <button className=
-                    'comment-submit' type='submit'>Submit Comment</button>
-                </form>
-                <div className='show-comments-wrapper'>
-                <ul>
-                    {Object.values(comments).map(comment => {
-                        return(
-                            <li key={comment._id}>
-                                {comment.username}
-                                <br/>
-                                {comment.body}
-                                <br/>
-                                {comment.rating}
-                            </li>
-                        );
-                    })}
-                </ul>
+              <br></br>
+              <label className="comment-textarea-label">Comment:</label>
+              <br></br>
+              <br></br>
+              <textarea
+                cols="40"
+                rows="3"
+                value={this.state.body}
+                onChange={this.update("body")}
+                placeholder="Tell everyone about your experiences!"
+                required
+                className="comment-textarea"
+              />
+              <br></br>
+              <button className="comment-submit" type="submit">
+                Submit Comment
+              </button>
+            </form>
+            <div className="show-comments-wrapper">
+              <ul>
+                {Object.values(comments).map((comment) => {
+                  return (
+                    <li key={comment._id}>
+                      {comment.username}
+                      <br />
+                      {comment.body}
+                      <br />
+                      {comment.rating}
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-        )
+          </div>
+        );
     }
 }
