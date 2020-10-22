c521be5653f3dc072cf16d80b5363099330c0078
cvidos <catherine.vidos@gmail.com>
2020-10-22 16:34:29 -0400

    profile page finished

--- frontend/src/components/user_profile/favspots.jsx
+++ frontend/src/components/user_profile/favspots.jsx
@@ -7,29 +7,72 @@ export default class FavSpots extends React.Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.subwayConvert = this.subwayConvert.bind(this);
+    this.subwayLine = this.subwayLine.bind(this);
+    this.handleButton = this.handleButton.bind(this);
+    this.toggleFav = this.toggleFav.bind(this);
   }
 
   handleClick() {
     this.props.logoutUser();
   }
 
-  // fetchComments() {
-  //   this.props.fetchComments
-  // }
+  handleButton(e) {
+    e.preventDefault();
+
+    const stopId = e.target.value;
+    this.toggleFav(stopId);
+  }
+
+  toggleFav(stopId) {
+
+    this.props.Favorite(stopId).then(() => {
+      this.props.saveFavStop(stopId);
+    });
+  }
 
   componentDidMount() {
     this.props.fetchAllStops();
-    console.log(this.props.stops[0]);
   }
 
-  
 
+  subwayConvert(stopColor) {
+    let icon;
+    if (stopColor === 'red') {
+      icon = 'subway-icon mta-red';
+    } else if (stopColor === 'green') {
+      icon = 'subway-icon mta-green';
+    } else if (stopColor === 'yellow') {
+      icon = 'subway-icon mta-yellow';
+    } else {
+      icon = '';
+    }
+    return icon;
+  }
+
+  subwayLine(stopColor) {
+    let lines;
+    if (stopColor === 'red') {
+      lines = [1, 2, 3];
+    } else if (stopColor === 'green') {
+      lines = [4, 5, 6];
+    } else if (stopColor === 'yellow') {
+      lines = ['N', 'Q', 'R'];
+    } else {
+      lines = ''
+    }
+    return lines;
+  }
   
 
     render() {
-      debugger
+      
+      let noStopsYet;
       if (this.props.stops.length === 0) {
+        noStopsYet = <h1 className='no-faves-yet'>You don't have any favorite stops yet...</h1>
         return null;
+      } else {
+        noStopsYet = <h1>My Favorites</h1>
       }
       // if (this.props.currentUser.favSpots === undefined) return null;
         // return (
@@ -56,9 +99,9 @@ export default class FavSpots extends React.Component {
                       <Link to="/">home</Link>
                     </div>
                     <div className="about-button">
-                      <Link to="https://github.com/catherinevidos/Desserts-Drinks">
+                      <a href="https://github.com/catherinevidos/Desserts-Drinks" target="_blank" >
                         about
-                      </Link>
+                      </a>
                     </div>
                     <button
                       className="logout-button-profile"
@@ -78,30 +121,35 @@ export default class FavSpots extends React.Component {
                     <li className="username-text">
                       {this.props.currentUser.username}
                     </li>
-                    <li>favorite desserts:</li>
+                    <li>desserts of the month:</li>
                     <ul className="desserts-list">
                       <li>macarons, cupcakes, petit fours</li>
                     </ul>
-                    <li className="drinks-list-parent">favorite drinks:</li>
+                    <li className="drinks-list-parent">drinks of the month:</li>
                     <ul className="drinks-list">
                       <li>cucumber spritz, hibiscus margarita, sancerre</li>
                     </ul>
                   </ul>
                 </div>
                 <div className="profile-form">
-                  <h1>My Favorites</h1>
+                  {noStopsYet}
                   <div className="stop-profile-wrapper">
                     <div className="stop-profile-header">
-                      <h1>Our Users Say...</h1>
                     </div>
                     <ul>
                       {this.props.stops[0].map((stop) => {
-                        debugger;
                         if (this.props.faves.includes(stop._id)) {
                           return (
                             <div className="fave-wrapper">
-                              <li className="fave-usernames">{stop.name} </li>
-                              <li className="stop-comments"> {stop.color} </li>
+                              <div className="delete-stop">
+                                <li className="fave-usernames">Stop: {stop.name} </li>
+                                <button onClick={this.handleButton} className='x-close-profile' value={stop._id}>X</button>
+                              </div>
+                          <li className='subway-line'>Line:
+                          <span className={this.subwayConvert(stop.color)}>{this.subwayLine(stop.color)[0]}</span>
+                          <span className={this.subwayConvert(stop.color)}>{this.subwayLine(stop.color)[1]}</span>
+                          <span className={this.subwayConvert(stop.color)}>{this.subwayLine(stop.color)[2]}</span>
+                          </li>
                             </div>
                           );
                         }
